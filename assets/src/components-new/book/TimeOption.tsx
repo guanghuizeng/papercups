884cfb8f915f550044511b8043edc3a341853fb5
Ivan Zen <gzwsed@gmail.com>
2021-01-10 15:45:26 +0800

    drafts

--- assets/src/components-new/book/TimeOption.tsx
+++ assets/src/components-new/book/TimeOption.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 function TimeOption({value, checked, onCheck, onConfirm}: any) {
   return (
@@ -9,7 +10,7 @@ function TimeOption({value, checked, onCheck, onConfirm}: any) {
           style={{width: 230, height: 52}}
           className="cursor-pointer mb-3 px-2 py-2 bg-white text-lightblue-500 font-bold border hover:shadow-blue-outline border-lightblue-500 rounded text-center"
         >
-          {value}
+          {value ? moment(value).format('HH:mm') : ''}
         </div>
       ) : (
         <div
@@ -17,7 +18,7 @@ function TimeOption({value, checked, onCheck, onConfirm}: any) {
           className="grid grid-cols-2 gap-2"
         >
           <div className="cursor-pointer mb-3 px-2 py-2 bg-gray-600 text-white font-bold  rounded text-center">
-            {value}
+            {value ? moment(value).format('HH:mm') : ''}
           </div>
           <div
             className="cursor-pointer mb-3 px-2 py-2 bg-lightblue-500 text-white font-bold border border-lightblue-500 rounded text-center"
