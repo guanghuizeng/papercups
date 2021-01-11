bd069c4177ba487ffbf81d589cd978db76a519c1
Ivan Zen <gzwsed@gmail.com>
2021-01-11 17:27:13 +0800

    drafts

--- lib/chat_api_web/booking_controller.ex
+++ lib/chat_api_web/booking_controller.ex
@@ -5,6 +5,59 @@ defmodule ChatApiWeb.BookingController do
   alias ChatApi.Events
   alias ChatApi.Events.Event
 
+  alias ChatApi.Schedules
+  alias ChatApi.Schedules.Schedule
+
+
+  # ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+  def weekday_to_name(weekday) do
+    Enum.at(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"], weekday)
+  end
+
+  def get_spots_per_duration(from_time, to_time, duration) do
+    if NaiveDateTime.diff(from_time, to_time) > 0 do
+      []
+    else
+      [from_time | get_spots_per_duration(NaiveDateTime.add(from_time, duration), to_time, duration)]
+    end
+  end
+
+  def get_spots_per_day(date, final, rules) do
+    if Date.diff(date, final) == 0 do
+      # stop
+      []
+    else
+      # 根据 day_of_weekday(date), 查询 rule
+      # 如果查到，则应用得到 spots
+      # 否则，unavailable
+      weekday = weekday_to_name(Date.day_of_week(date))
+      rule = Enum.find(rules, fn (x) -> x["wday"] == weekday end)
+      if rule do
+        list_of_spots = Enum.map(
+          rule["intervals"],
+          fn (x) ->
+            %{"from" => from, "to" => to} = x
+            {:ok, from_time} = NaiveDateTime.from_iso8601(Date.to_iso8601(date) <> " " <> from <> ":00")
+            {:ok, to_time} = NaiveDateTime.from_iso8601(Date.to_iso8601(date) <> " " <> to <> ":00")
+            spots = get_spots_per_duration(from_time, to_time, 15 * 60)
+            spots
+          end
+        )
+        [
+          %{
+            date: Date.to_iso8601(date),
+            status: "available",
+            spots: list_of_spots
+          }
+          |
+          get_spots_per_day(Date.add(date, 1), final, rules)
+        ]
+      else
+        get_spots_per_day(Date.add(date, 1), final, rules)
+      end
+    end
+  end
+
   @doc """
   Get available spots.
 
@@ -13,13 +66,29 @@ defmodule ChatApiWeb.BookingController do
   """
   @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def show(conn, params) do
-    %{"event_type_id" => event_type_id, "start_time" => start_time, "end_time" => end_time } = params
+    %{"event_type_id" => event_type_id, "start_time" => start_time, "end_time" => end_time} = params
     # event_type_id => schedule
     # start_time & end_time => list of days
     # date => day of week => rule => spots
     # query events by time
     # remove occupied time from spots
     # return spots
+
+#    with {:ok, %EventType{} = event_type} <- Events.get_event!(event_type_id) do
+#      with {:ok, %Schedule{} = schedule} <- Schedules.get_schedule!(event_type.schedule_id) do
+#        {:ok, date} = Date.from_iso8601(start_time)
+#        {:ok, end_date} = Date.from_iso8601(end_time)
+#        rules = JSON.decode(schedule.rules)
+#        spots = get_spots_per_day(date, end_time, rules)
+#        json(
+#          conn,
+#          %{
+#            ok: true,
+#            data: spots
+#          }
+#        )
+#      end
+#    end
 
     json(
       conn,
