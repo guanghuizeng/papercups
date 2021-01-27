7d25f39ef7420f259f7c273a67b87259084e7104
Ivan Zen <gzwsed@gmail.com>
2021-01-27 12:35:59 +0800

    drafts

--- assets/src/components-new/booking/BookingPage.tsx
+++ assets/src/components-new/booking/BookingPage.tsx
@@ -9,26 +9,121 @@ function GeneralSection() {
 
   return (
     <div>
-      <div>{user?.display_name}</div>
+      <div>{schedulingLink?.organizer.displayName}</div>
+      <div>{schedulingLink?.name}</div>
       <div>{schedulingLink?.name}</div>
       <div>{schedulingLink?.description}</div>
 
-      <div>{schedulingLink?.organizer.displayName}</div>
       <div>
         {schedulingLink?.durations.map((d: number) => {
           return <div key={d}>{d}</div>;
         })}
       </div>
       <div>{schedulingLink?.location}</div>
-      <div>Signed in as Yuanyuan Zhang</div>
+
+      <div>Signed in as {user?.display_name}</div>
+    </div>
+  );
+}
+
+function EventSection() {
+  const {user, timeSelected, schedulingLink, setEventTime} = useBooking();
+
+  return (
+    <div className="pt-2">
+      <div>
+        <div>{schedulingLink?.organizer.displayName}</div>
+        <div>{schedulingLink?.name}</div>
+        <div>{schedulingLink?.name}</div>
+        <div>{schedulingLink?.description}</div>
+
+        <div>
+          {schedulingLink?.durations.map((d: number) => {
+            return <div key={d}>{d}</div>;
+          })}
+        </div>
+        <div>{schedulingLink?.location}</div>
+
+        <div>Start: {timeSelected?.start}</div>
+        <div>End: {timeSelected?.end}</div>
+      </div>
+
+      <div className="pt-2">
+        <div>联系人</div>
+        <input
+          className="border-primary border-solid border"
+          placeholder=""
+          type="text"
+          id={'name'}
+        />
+      </div>
+      <div className="pt-2">
+        <div>Email</div>
+        <input
+          className="border-primary border-solid border"
+          placeholder=""
+          type="email"
+          id={'email'}
+        />
+      </div>
+      {schedulingLink?.fields && (
+        <div>
+          {schedulingLink.fields.map((field: any) => {
+            return (
+              <div key={field.id} className="pt-2">
+                <label>{field.label}</label>
+                <input
+                  className="border-primary border-solid border"
+                  placeholder=""
+                  type="text"
+                  id={field.id}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="flex flex-row pt-2">
+        <div className="btn-draft">确定</div>
+        <div className="btn-draft" onClick={() => {}}>
+          取消
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CalendarMonthView() {
+  return <div>Month view</div>;
+}
+
+function ControlSection() {
+  const {timeSelected} = useBooking();
+  return (
+    <div className="flex flex-col w-72">
+      <GeneralSection />
+      <div className={'border-primary border-2 border-solid'} />
+      <EventSection />
+      <div className={'border-primary border-2 border-solid'} />
+      <CalendarMonthView />
     </div>
   );
 }
 
 function CalendarSection() {
-  const {userSlug, schedulingLinkSlug} = useBooking();
+  const {setEventTime} = useBooking();
 
-  return <div>Calendar for {schedulingLinkSlug}</div>;
+  return (
+    <div>
+      <div
+        onClick={() => {
+          setEventTime('2021-01-28T09:00:00', '2021-01-28T10:00:00');
+        }}
+      >
+        Set time
+      </div>
+    </div>
+  );
 }
 
 export default function BookingPage() {
@@ -47,9 +142,7 @@ export default function BookingPage() {
       schedulingLinkSlug={schedulingLinkSlug}
     >
       <div className="flex flex-row">
-        <div className="flex flex-col w-72">
-          <GeneralSection />
-        </div>
+        <ControlSection />
         <div>
           <CalendarSection />
         </div>
--- assets/src/components-new/booking/BookingProvider.tsx
+++ assets/src/components-new/booking/BookingProvider.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useAuth} from '../../components/auth/AuthProvider';
 import {
   useLink,
@@ -6,19 +6,31 @@ import {
   useUserProfileBySlug,
 } from '../../api-hooks';
 import {useSchedulingLink} from '../../hooks/SchedulingLinkProvider';
+import {Dayjs} from 'dayjs';
+
+interface EventTime {
+  start: string;
+  end: string;
+}
 
 const BookingContext = React.createContext<{
   userSlug: string;
   schedulingLinkSlug: string;
+  timeSelected: EventTime | null;
 
   user: any;
   schedulingLink: any;
+  createScheduledEvent: () => Promise<any>;
+  setEventTime: (start: string, end: string) => void;
 }>({
   userSlug: '',
   schedulingLinkSlug: '',
+  timeSelected: null,
 
   user: {},
   schedulingLink: {},
+  createScheduledEvent: () => Promise.resolve(),
+  setEventTime: (start: string, end: string) => {},
 });
 
 export const useBooking = () => useContext(BookingContext);
@@ -32,24 +44,33 @@ type Props = React.PropsWithChildren<BookingProviderProps>;
 
 function BookingProvider(props: Props) {
   const {userSlug, schedulingLinkSlug} = props;
-
-  // userSlug, schedulingLinkSlug
-  // => scheduling link information
-
   const {data: user} = useUserProfileBySlug(userSlug);
   const {data: schedulingLink} = useSchedulingLinkBySlug(
     userSlug,
     schedulingLinkSlug
   );
+  const [timeSelected, setTimeSelected] = useState<EventTime | null>(null);
+
+  const createScheduledEvent = async () => {
+    return Promise.resolve();
+  };
+
+  const setEventTime = (start: string, end: string) => {
+    setTimeSelected({start, end});
+  };
 
   return (
     <BookingContext.Provider
       value={{
-        userSlug: userSlug,
-        schedulingLinkSlug: schedulingLinkSlug,
+        userSlug,
+        schedulingLinkSlug,
+        timeSelected,
 
         user,
         schedulingLink,
+        createScheduledEvent,
+
+        setEventTime,
       }}
     >
       {props.children}
