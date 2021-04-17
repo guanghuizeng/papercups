0a9ed62773ba80edbf8e09f8dec9e2dd49e58bf4
Guanghui Zeng <gzwsed@gmail.com>
2021-04-17 13:47:15 +0800

    update

--- assets/src/components-new/SchedulingLinksBoard.tsx
+++ assets/src/components-new/SchedulingLinksBoard.tsx
@@ -1,47 +1,11 @@
-import React, {useEffect} from 'react';
-import {
-  SearchBox,
-  ISearchBoxStyles,
-  DefaultButton,
-  PrimaryButton,
-} from '@fluentui/react';
+import React from 'react';
 import {useAppData} from '../hooks/AppDataProvider';
-import EventTypeCard from './EventTypeCard';
-import SchedulingLinkCard from './SchedulingLinkCard';
-import {Link, useLocation} from 'react-router-dom';
-import * as API from '../api';
-import {useAuth} from '../components/auth/AuthProvider';
+import {Link} from 'react-router-dom';
 import NavSidebar from './NavSidebar';
-
-const searchBoxStyles: Partial<ISearchBoxStyles> = {
-  root: {width: 200, border: '0'},
-};
-
-export const SearchBoxSmallExample = () => (
-  <SearchBox
-    styles={searchBoxStyles}
-    placeholder="Search"
-    onEscape={(ev) => {
-      console.log('Custom onEscape Called');
-    }}
-    onClear={(ev) => {
-      console.log('Custom onClear Called');
-    }}
-    onChange={(_, newValue) =>
-      console.log('SearchBox onChange fired: ' + newValue)
-    }
-    onSearch={(newValue) =>
-      console.log('SearchBox onSearch fired: ' + newValue)
-    }
-  />
-);
+import {Button} from '@geist-ui/react';
 
 export function SchedulingLinksBoard() {
-  const {
-    schedulingLinks,
-    createSchedulingLinkAndRedirect,
-    availabilityPresets,
-  } = useAppData();
+  const {schedulingLinks, createSchedulingLinkAndRedirect} = useAppData();
 
   return (
     <div className="w-full flex flex-row">
@@ -72,57 +36,36 @@ export function SchedulingLinksBoard() {
               New
             </button>
           </div>
-          {/*<div className="flex flex-row mb-2">*/}
-          {/*  <Link to="/links/add" className="cursor-pointer mr-2 py-1 ">*/}
-          {/*    <DefaultButton text="Add Event Type" />*/}
-          {/*  </Link>*/}
-          {/*  <div className="cursor-pointer mr-2 py-1 ">*/}
-          {/*    <DefaultButton text="Copy Link" />*/}
-          {/*  </div>*/}
-          {/*  <div className="cursor-pointer  py-1 ">*/}
-          {/*    <DefaultButton text="Add to Website" />*/}
-          {/*  </div>*/}
-          {/*</div>*/}
-          {/*<div className="gentle-flex mb-2">*/}
-          {/*  <SearchBoxSmallExample />*/}
-          {/*</div>*/}
         </div>
-        <div className="flex flex-row flex-wrap">
+        <div className="grid grid-cols-4 gap-y-1 flex-wrap">
           {schedulingLinks &&
-            schedulingLinks
-              // .sort((link1: any, link2: any) => link1.created_at < link2.created_at)
-              .map((link: any) => {
-                return (
-                  <div key={link.id} className="py-4 pl-4 mb-4 w-64 h-32">
-                    <SchedulingLinkCard schedulingLink={link} />
+            schedulingLinks.map((link: any) => {
+              const {id, durations, url, name} = link;
+              console.log('link', link);
+              return (
+                <React.Fragment key={id}>
+                  <div className="px-3 py-1">{name}</div>
+                  <div className="px-3 py-1">
+                    {durations.map((d: number) => (
+                      <span>{d}</span>
+                    ))}
+                    分钟
                   </div>
-                );
-              })}
-          <div className="py-4 pl-4 w-64 h-32">
-            {/*<button className="w-full h-full hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2">*/}
-            {/*    <svg*/}
-            {/*        className="group-hover:text-light-blue-600 text-light-blue-500 mr-2"*/}
-            {/*        width="12"*/}
-            {/*        height="20"*/}
-            {/*        fill="currentColor"*/}
-            {/*    >*/}
-            {/*        <path*/}
-            {/*            fill-rule="evenodd"*/}
-            {/*            clip-rule="evenodd"*/}
-            {/*            d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"*/}
-            {/*        />*/}
-            {/*    </svg>*/}
-            {/*    New*/}
-            {/*</button>*/}
-            <li className="hover:shadow-lg flex hover:bg-light-blue-200 hover:text-light-blue-800 rounded-lg w-full h-full text-light-blue-600">
-              <div
-                onClick={createSchedulingLinkAndRedirect}
-                className="cursor-pointer hover:border-transparent hover:shadow-xs w-full flex items-center justify-center rounded-lg border-2 border-dashed border-gray-200 text-sm font-medium py-4"
-              >
-                新建
-              </div>
-            </li>
-          </div>
+                  <div className="px-3 py-1 text-blue-500 hover:bg-blue-50 cursor-pointer rounded-md ">
+                    /{url}
+                  </div>
+
+                  <div className="px-3 py-1">
+                    <Link to={`/links/${id}`}>
+                      <Button size="mini">编辑</Button>
+                    </Link>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+        </div>
+        <div>
+          <Button onClick={createSchedulingLinkAndRedirect}>新建</Button>
         </div>
       </div>
     </div>
