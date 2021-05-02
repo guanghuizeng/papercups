aa4de6695245d20ac588de2cc2326a2e34e39637
Guanghui Zeng <gzwsed@gmail.com>
2021-05-02 14:11:10 +0800

    refactor

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -6,20 +6,20 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import {useAuth} from './components-new/auth/AuthProvider';
-import Login from './components-new/auth/Login';
-import Register from './components-new/auth/Register';
-import EmailVerification from './components-new/auth/EmailVerification';
-import PasswordReset from './components-new/auth/PasswordReset';
-import RequestPasswordReset from './components-new/auth/RequestPasswordReset';
-import PasswordResetRequested from './components-new/auth/PasswordResetRequested';
-import Dashboard from './components-new/Dashboard';
+import {useAuth} from './components/auth/AuthProvider';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import EmailVerification from './components/auth/EmailVerification';
+import PasswordReset from './components/auth/PasswordReset';
+import RequestPasswordReset from './components/auth/RequestPasswordReset';
+import PasswordResetRequested from './components/auth/PasswordResetRequested';
+import Dashboard from './components/Dashboard';
 import './App.css';
 import './tailwind.css';
-import LandingPage from './components-new/landing/LandingPage';
+import LandingPage from './components/landing/LandingPage';
 import {initializeIcons} from '@fluentui/react';
-import BookingPage from './components-new/booking/BookingPage';
-import ScheduledEvent from './components-new/ScheduledEvent';
+import BookingPage from './components/booking/BookingPage';
+import ScheduledEvent from './components/ScheduledEvent';
 
 initializeIcons();
 
