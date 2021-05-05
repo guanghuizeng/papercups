49943c90fd2b9b2db20382563496a8db2908d7a8
Guanghui Zeng <gzwsed@gmail.com>
2021-05-05 11:50:26 +0800

    refactor

--- assets/src/components/common/Editor.tsx
+++
@@ -1,8 +0,0 @@
-import 'react-quill/dist/quill.snow.css';
-import EditorCore from './EditorCore';
-
-// const DynamicComponentWithNoSSR = dynamic(() => import("./EditorCore"), {
-//   ssr: false,
-// });
-
-export default EditorCore;
