86a4ec0e393a303e7baf409ec35937aba6ad8d49
Guanghui Zeng <gzwsed@gmail.com>
2021-05-05 12:31:20 +0800

    refactor

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -13,12 +13,17 @@ import EmailVerification from './components/auth/EmailVerification';
 import PasswordReset from './components/auth/PasswordReset';
 import RequestPasswordReset from './components/auth/RequestPasswordReset';
 import PasswordResetRequested from './components/auth/PasswordResetRequested';
-import Dashboard from './components/Dashboard';
 import './styles/tailwind.css';
 import LandingPage from './components/landing/LandingPage';
 import {initializeIcons} from '@fluentui/react';
 import BookingPage from './components/booking/BookingPage';
 import ScheduledEvent from './components/events/ScheduledEvent';
+import AppDataProvider from './hooks/AppDataProvider';
+import {SchedulingLinksBoard} from './components/links/SchedulingLinksBoard';
+import SchedulingLink from './components/links/SchedulingLink';
+import ScheduledEvents from './components/events/ScheduledEvents';
+import {AppSettings} from './components/settings/AppSettings';
+import {Availability} from './components/availability/Availability';
 
 initializeIcons();
 
@@ -47,7 +52,7 @@ const App = () => {
           />
           <Route path="/event/:id" component={ScheduledEvent} />
           <Route
-            path="/@:userSlug/:schedulingLinkSlug"
+            path="/:userSlug/:schedulingLinkSlug"
             component={BookingPage}
           />
           <Route
@@ -75,8 +80,45 @@ const App = () => {
           path="/reset-password-requested"
           component={PasswordResetRequested}
         />
-        <Route path="/event/:id" component={ScheduledEvent} />
-        <Route path="/" component={Dashboard} />
+        <AppDataProvider>
+          <div
+            className="flex flex-col md:flex-row h-full w-screen"
+            style={{
+              minHeight: '100vh',
+            }}
+          >
+            <div className="flex flex-row w-full">
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  component={() => <Redirect to={'/links'} />}
+                />
+                <Route exact path="/links" component={SchedulingLinksBoard} />
+                <Route
+                  path="/links/:id/edit"
+                  component={() => (
+                    <div className="w-full border-l border-gray-200 border-solid">
+                      <SchedulingLink />
+                    </div>
+                  )}
+                />
+                <Route path="/events" component={ScheduledEvents} />
+                <Route path="/event/:id" component={ScheduledEvent} />
+                <Route path="/settings" component={AppSettings} />
+                <Route
+                  path="/availabilities/:id/edit"
+                  component={Availability}
+                />
+                {/*<Route path="/apps" component={() => <div>apps</div>} />*/}
+                <Route
+                  path="/:userSlug/:schedulingLinkSlug"
+                  component={BookingPage}
+                />
+              </Switch>
+            </div>
+          </div>
+        </AppDataProvider>
       </Switch>
     </Router>
   );
